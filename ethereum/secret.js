3813e9e2cce538b652aa2d4454245261dc239657
nikunj chaudhari <31696023+nikunj321@users.noreply.github.com>
2020-11-01 11:21:27 +0530

    Delete secret.js

--- ethereum/secret.js
+++
@@ -1,5 +0,0 @@
-export default {
-    api : 'https://rinkeby.infura.io/v3/482ab3d859be441ab018517f2654b346',
-    mnemonics : 'sing observe organ case power brief crash tone tissue winter clog genius'          
-    
-};
